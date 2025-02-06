2d0cb823b1224b1eb326491b9cdb204a73bc9d00
Jackson2706 <dung27062002@gmail.com>
2025-02-06 21:49:43 +0700

    complete

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 mkdir ./weights
 cd ./weights 
 wget https://huggingface.co/facebook/sapiens-pretrain-0.3b-torchscript/resolve/main/sapiens_0.3b_epoch_1600_torchscript.pt2
-wget 
+cd ..
+
+python3 inference
